bba554aac72edbd0789d4e0e8239de17f6c9f07a
Aero1608 <joao3021@hotmail.com>
2022-06-07 11:54:02 -0500

    [FIX]Typo

--- account_move_line_product_name/models/res_partner.py
+++ account_move_line_product_name/models/res_partner.py
@@ -2,7 +2,7 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.parnter'
+    _inherit = 'res.partner'
     
     invoice_document_type = fields.Char(string="Tipo de documento elegido por el cliente")
     
