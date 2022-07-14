47de1df570ce49d751458f67f3b39fe8d33c921f
Aero1608 <joao3021@hotmail.com>
2022-07-14 09:51:09 -0500

    [ADD]Selection field

--- account_move_line_product_name/models/res_partner.py
+++ account_move_line_product_name/models/res_partner.py
@@ -3,5 +3,5 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    invoice_document_type = fields.Char(string="Tipo de documento elegido por el cliente")
+    invoice_document_type = fields.Selection([('boleta','Boleta'),('factura','Factura')],"Tipo de documento elegido por el cliente")
     
